c4790beb2bb147d0e126f8359c78c1b8523502f9
kajal-shar <147511175+kajal-shar@users.noreply.github.com>
2023-12-06 22:14:22 +0530

    Add files via upload

---
+++ evens.java
@@ -0,0 +1,12 @@
+public class evens{
+    public static void main(String args[]){
+        for (inti=2;i<=n;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+
+
+    }
+}
+
